256197f7745d981220853fe9ea9d05bdaa8767b2
Rumeleq <100099007+Rumeleq@users.noreply.github.com>
2025-05-06 17:08:16 +0200

    moved frontend sql query to newly created fastapi backend

--- frontend/main.py
+++ frontend/main.py
@@ -1,28 +1,19 @@
-import sqlite3
-
 import pandas as pd
+import requests
 import streamlit as st
 
 st.set_page_config(page_title="Hospital bed management", page_icon="🏥")
 
 st.title("Bed Assignments")
 
-conn = sqlite3.connect("./db/hospital.db")
+response = requests.get("http://localhost:8000/get-bed-assignments")
+if response.status_code == 200:
+    df = pd.DataFrame(response.json())
 
-query = """
-SELECT
-    bed_assignments.bed_id,
-    bed_assignments.patient_id,
-    CONCAT(patients.first_name, ' ', patients.last_name) AS patient_name,
-    patients.sickness,
-    bed_assignments.days_of_stay
-FROM bed_assignments
-JOIN patients ON bed_assignments.patient_id = patients.patient_id;
-"""
-df = pd.read_sql_query(query, conn)
-conn.close()
-
-if not df.empty:
-    st.dataframe(df, use_container_width=True)
+    if not df.empty:
+        st.dataframe(df, use_container_width=True)
+    else:
+        st.info("No bed assignments found.")
 else:
-    st.info("No bed assignments found.")
+    st.error("Failed to fetch data from the server.")
+    print(response.status_code)
