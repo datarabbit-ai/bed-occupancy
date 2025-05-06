273611c45cc9c4e8306b97007ebb6d4d1674de55
JanTopolewski <jankotopolewski@gmail.com>
2025-05-06 13:16:12 +0200

    Solves problem with pre-commit checks

--- faker/init_database.sql
+++ faker/init_database.sql
@@ -6,20 +6,17 @@ CREATE TABLE IF NOT EXISTS patients (
     contact_phone CHAR(9) NOT NULL,
     sickness TEXT NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS patient_queue (
     patient_id INTEGER NOT NULL,
     queue_id INTEGER NOT NULL UNIQUE,
     will_come TINYINT(1) NOT NULL CHECK (will_come IN (0, 1)),
     FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
 );
-
 CREATE TABLE IF NOT EXISTS beds (
     bed_id INTEGER PRIMARY KEY
 );
-
 CREATE TABLE IF NOT EXISTS bed_assignments (
-    bed_id INTEGER UNIQUE NOT NULL, 
+    bed_id INTEGER UNIQUE NOT NULL,
     patient_id INTEGER UNIQUE NOT NULL,
     days_of_stay INTEGER UNSIGNED NOT NULL,
     FOREIGN KEY (bed_id) REFERENCES beds(bed_id),
