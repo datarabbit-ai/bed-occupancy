2f05033d2f65612fc4f0257c6be77fbec1f8af2c
Rumeleq <100099007+Rumeleq@users.noreply.github.com>
2025-05-07 15:50:15 +0200

    initial outline of frontend for simulation

--- frontend/main.py
+++ frontend/main.py
@@ -3,17 +3,25 @@ import requests
 import streamlit as st
 
 st.set_page_config(page_title="Hospital bed management", page_icon="🏥")
-
 st.title("Bed Assignments")
 
-response = requests.get("backend:8000/get-bed-assignments")
-if response.status_code == 200:
-    df = pd.DataFrame(response.json())
 
-    if not df.empty:
-        st.dataframe(df, use_container_width=True)
+def get_bed_assignments() -> pd.DataFrame:
+    response = requests.get("http://backend:8000/get-bed-assignments")
+    if response.status_code == 200:
+        return pd.DataFrame(response.json())
     else:
-        st.info("No bed assignments found.")
+        st.error("Failed to fetch data from the server.")
+        return pd.DataFrame()
+
+
+if st.button("➡️ Simulate Next Day"):
+    response = requests.post("http://backend:8000/simulate-next-day")
+    if response.status_code != 200:
+        st.error("Failed to simulate next day.")
+
+df = get_bed_assignments()
+if not df.empty:
+    st.dataframe(df, use_container_width=True)
 else:
-    st.error("Failed to fetch data from the server.")
-    print(response.status_code)
+    st.info("No bed assignments found.")
