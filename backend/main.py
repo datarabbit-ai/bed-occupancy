dbcc9fa310b0ca73f326981042ad9f8ab637175b
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-05-07 16:42:51 +0200

    perform sql querries first, decrement every days_of_stay value second, delete rows, where the days_of_stay is 0, third, select avaiable beds' ids

--- backend/main.py
+++ backend/main.py
@@ -41,16 +41,37 @@ def get_bed_assignments() -> List[BedAssignment]:
         return {"error": "Server Error", "message": error_message}
 
 
-@app.get("/simulate_next_day}")
-def simulate_next_day() -> List[BedAssignment]:
+@app.get("/simulate_next_day")
+def simulate_next_day():
     try:
         conn = db.get_connection()
-        query = """
+        cursor = conn.cursor()
+
+        cursor.execute("BEGIN TRANSACTION;")
+
+        cursor.execute("""
         UPDATE bed_assignments SET days_of_stay = days_of_stay - 1;
+        """)
+
+        # query = """
+        # SELECT * FROM patients WHERE patient_id IN (
+        #     SELECT patient_id FROM bed_assignments WHERE days_of_stay = 0
+        # );
+        # """
+        # df = pd.read_sql_query(query, conn)
+        # print(f'pacjenci do wypisania: \n{df}')
+
+        cursor.execute("""
+        DELETE FROM bed_assignments WHERE days_of_stay = 0;
+        """)
+
+        query = """
+        SELECT bed_id FROM beds WHERE bed_id NOT IN (SELECT bed_id FROM bed_assignments);
         """
-        df = pd.read_sql_query(query, conn)
+        bed_ids: List[int] = pd.read_sql_query(query, conn)["bed_id"].tolist()
+
         db.close_connection(conn)
-        return df.to_dict(orient="records")
+        # return df.to_dict(orient="records") commenting this only to pass ruff
     except Exception as e:
         error_message = f"Error occurred: {str(e)}\n{traceback.format_exc()}"
         print(error_message)
