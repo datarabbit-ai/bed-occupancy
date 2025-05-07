0cdf00dfb5a6948783eab893ce8574889593d34a
Rumeleq <100099007+Rumeleq@users.noreply.github.com>
2025-05-07 20:24:57 +0200

    simulation for days 1 to 20

--- backend/db_operations.py
+++ backend/db_operations.py
@@ -13,7 +13,6 @@ def get_connection() -> sqlite3.Connection:
     try:
         conn = sqlite3.connect(db_path)
         conn.row_factory = sqlite3.Row
-        conn.cursor().execute("BEGIN TRANSACTION;")
         return conn
     except Exception as e:
         print(f"Failed to connect: {e}")
--- backend/main.py
+++ backend/main.py
@@ -25,12 +25,10 @@ day_for_simulation = 1
 @app.get("/update-day")
 def update_day(delta: int = fastapi.Query(...)):
     global day_for_simulation
-    print(day_for_simulation)
     if delta not in (-1, 1):
         return {"error": "Invalid delta value. Use -1 or 1."}
     if delta == 1 and day_for_simulation < 20 or delta == -1 and day_for_simulation > 1:
         day_for_simulation += delta
-    print(day_for_simulation)
     return {"day": day_for_simulation}
 
 
@@ -38,98 +36,87 @@ def update_day(delta: int = fastapi.Query(...)):
 def get_bed_assignments() -> List[BedAssignment]:
     try:
         conn = db.get_connection()
-        query = """
-        SELECT
-            bed_assignments.bed_id,
-            bed_assignments.patient_id,
-            patients.first_name || ' ' || patients.last_name AS patient_name,
-            patients.sickness,
-            bed_assignments.days_of_stay
-        FROM bed_assignments
-        JOIN patients ON bed_assignments.patient_id = patients.patient_id;
-        """
-        df = pd.read_sql_query(query, conn)
-        db.close_connection(conn)
-        return df.to_dict(orient="records")
-    except Exception as e:
-        error_message = f"Error occurred: {str(e)}\n{traceback.format_exc()}"
-        print(error_message)
-        return {"error": "Server Error", "message": error_message}
-
-
-def simulate_for_day(day: int) -> List[BedAssignment]:
-    try:
-        conn = db.get_connection()
         cursor = conn.cursor()
 
         cursor.execute("BEGIN TRANSACTION;")
 
-        cursor.execute("""
-        UPDATE bed_assignments SET days_of_stay = days_of_stay - 1;
-        """)
+        for _ in range(day_for_simulation - 1):
+            cursor.execute("""
+                           UPDATE bed_assignments
+                           SET days_of_stay = days_of_stay - 1;
+                           """)
 
-        query = """
-        SELECT * FROM patients WHERE patient_id IN (
-            SELECT patient_id FROM bed_assignments WHERE days_of_stay = 0
-        );
-        """
-        df = pd.read_sql_query(query, conn)
-        print(f"pacjenci do zwolnienia: \n{df}")
+            query = """
+                    SELECT * \
+                    FROM patients \
+                    WHERE patient_id IN (SELECT patient_id \
+                                         FROM bed_assignments \
+                                         WHERE days_of_stay = 0); \
+                    """
+            df = pd.read_sql_query(query, conn)
+            print(f"pacjenci do zwolnienia: \n{df}")
 
-        cursor.execute("""
-        DELETE FROM bed_assignments WHERE days_of_stay = 0;
-        """)
+            cursor.execute("""
+                           DELETE
+                           FROM bed_assignments
+                           WHERE days_of_stay = 0;
+                           """)
 
-        query = """
-        SELECT bed_id FROM beds WHERE bed_id NOT IN (SELECT bed_id FROM bed_assignments);
-        """
-        bed_ids: List[int] = pd.read_sql_query(query, conn)["bed_id"].tolist()
+            query = """
+                    SELECT bed_id \
+                    FROM beds \
+                    WHERE bed_id NOT IN (SELECT bed_id FROM bed_assignments); \
+                    """
+            bed_ids: List[int] = pd.read_sql_query(query, conn)["bed_id"].tolist()
 
-        query = """
-        SELECT patient_id, queue_id FROM patient_queue
-        """
-        queue = pd.read_sql_query(query, conn)
+            query = """
+                    SELECT patient_id, queue_id \
+                    FROM patient_queue \
+                    """
+            queue = pd.read_sql_query(query, conn)
 
-        bed_iterator = 0
-        for patient in queue["patient_id"]:
-            if bed_iterator >= len(bed_ids):
-                break
+            bed_iterator = 0
+            for patient in queue["patient_id"]:
+                if bed_iterator >= len(bed_ids):
+                    break
 
-            will_come: bool = random.choice([True, True, True, True, False])
+                will_come: bool = random.choice([True, True, True, True, False])
 
-            if will_come:
-                try:
-                    days: int = random.randint(1, 10)
+                if will_come:
+                    try:
+                        days: int = random.randint(1, 10)
 
-                    cursor.execute(
-                        """
-                        INSERT INTO bed_assignments (bed_id, patient_id, days_of_stay) VALUES (?, ?, ?)
-                        """,
-                        (bed_ids[bed_iterator], patient, days),
-                    )
+                        cursor.execute(
+                            """
+                            INSERT INTO bed_assignments (bed_id, patient_id, days_of_stay)
+                            VALUES (?, ?, ?)
+                            """,
+                            (bed_ids[bed_iterator], patient, days),
+                        )
 
-                    print(f"pacjent o id {patient} dostał łóżko o id {bed_ids[bed_iterator]} na {days} dni")
-                    bed_iterator += 1
+                        print(f"pacjent o id {patient} dostał łóżko o id {bed_ids[bed_iterator]} na {days} dni")
+                        bed_iterator += 1
 
-                except sqlite3.IntegrityError:
-                    print(f"pomijanie pacjenta od id {patient}, gdyż już jest na łóżku")
-            else:
-                print(f"pacjent o id {patient} nie przyszedł")
+                    except sqlite3.IntegrityError:
+                        print(f"pomijanie pacjenta od id {patient}, gdyż już jest na łóżku")
+                else:
+                    print(f"pacjent o id {patient} nie przyszedł")
 
         # db.close_connection(conn)
         # return get_bed_assignments()
 
         query = """
-        SELECT
-            bed_assignments.bed_id,
-            bed_assignments.patient_id,
-            patients.first_name || ' ' || patients.last_name AS patient_name,
-            patients.sickness,
-            bed_assignments.days_of_stay
-        FROM bed_assignments
-        JOIN patients ON bed_assignments.patient_id = patients.patient_id;
+            SELECT bed_assignments.bed_id,
+                   bed_assignments.patient_id,
+                   patients.first_name || ' ' || patients.last_name AS patient_name,
+                   patients.sickness,
+                   bed_assignments.days_of_stay
+            FROM bed_assignments
+            JOIN patients ON bed_assignments.patient_id = patients.patient_id;
         """
         df = pd.read_sql_query(query, conn)
+
+        cursor.execute("ROLLBACK;")
         db.close_connection(conn)
         return df.to_dict(orient="records")
 
