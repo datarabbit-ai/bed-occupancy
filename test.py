862cea0bd99824e16c376f2e7b2476bfd1fca1b2
Rumeleq <100099007+Rumeleq@users.noreply.github.com>
2025-05-23 13:10:53 +0200

    cleanup

--- test.py
+++ test.py
@@ -1,41 +1,32 @@
 import os
 
-import requests
 from dotenv import load_dotenv
+from elevenlabs import ElevenLabs
 
 load_dotenv()
 
 ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 AGENT_ID = os.getenv("AGENT_ID")
-TWILIO_SID = os.getenv("TWILIO_SID")
-TWILIO_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 PHONE_TO_CALL = os.getenv("PHONE_TO_CALL")
 AGENT_PHONE_NUMBER_ID = os.getenv("AGENT_PHONE_NUMBER_ID")
 
-headers = {"xi-api-key": ELEVENLABS_API_KEY, "Content-Type": "application/json"}
+client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
 
-payload = {
-    "agent_id": AGENT_ID,
-    "conversation_initiation_client_data": {
-        "dynamic_variables": {
-            "patient_name": "Jan",
-            "patient_surname": "Topolewski",
-            "personal_number": "234",
-            "patient_sickness": "zapalenie kolana",
-            "current_visit_day": 10,
-            "suggested_appointment_day": 5,
-        }
-    },
-    "to_number": PHONE_TO_CALL,
-    "agent_phone_number_id": AGENT_PHONE_NUMBER_ID,
-    "twilio_account_sid": TWILIO_SID,
-    "twilio_auth_token": TWILIO_TOKEN,
+conversation_initiation_client_data = {
+    "dynamic_variables": {
+        "patient_name": "Michał",
+        "patient_surname": "Mikołajczak",
+        "personal_number": 234,
+        "patient_sickness": "zapalenie kolana",
+        "current_visit_day": 10,
+        "suggested_appointment_day": 5,
+    }
 }
 
-response = requests.post("https://api.elevenlabs.io/v1/convai/twilio/outbound_call", headers=headers, json=payload)
-
-if response.status_code == 200:
-    call_data = response.json()
-    print(f"Call ID: {call_data}")
-else:
-    print(f"Błąd: {response.text}")
+response = client.conversational_ai.twilio_outbound_call(
+    agent_id=AGENT_ID,
+    agent_phone_number_id=AGENT_PHONE_NUMBER_ID,
+    to_number=PHONE_TO_CALL,
+    conversation_initiation_client_data=conversation_initiation_client_data,
+)
+print(response.conversation_id)
