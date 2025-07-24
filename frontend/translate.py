8cb2fddfaa75d723eaaf3aa6fe6409ad4175134d
wiktor <wiktor.kycia@gmail.com>
2025-07-24 12:48:47 +0200

    use pydantic models as the model output schema, also convert python dicts to openai's weird objects in order for pycharm not to complain

--- frontend/translate.py
+++ frontend/translate.py
@@ -1,8 +1,13 @@
 import json
 import os
+from typing import Any
 
 from dotenv import load_dotenv
+from models import Transcript
 from openai import OpenAI
+from openai.types.responses.easy_input_message_param import EasyInputMessageParam
+from openai.types.responses.response_format_text_json_schema_config_param import ResponseFormatTextJSONSchemaConfigParam
+from openai.types.responses.response_text_config_param import ResponseTextConfigParam
 
 load_dotenv()
 
@@ -27,21 +32,29 @@ def translate(client: OpenAI, transcript: dict, lang: str) -> dict:
     :param lang: language to translate to
     :return: translated messages
     """
-    response = client.chat.completions.create(
+    model_schema: dict[str, Any] = Transcript.model_json_schema()
+    input_messages = [
+        EasyInputMessageParam(
+            role="system",
+            content=f"""You are a professional translator, your role is to translate a given transcript of a voice call between AI agent and human to the language: {lang}. \n
+                        Only return the translated transcript in the JSON format, nothing else. \n """
+            + 'The JSON format should look like this: {"transcript":[{"role":"<role>", "message":"<translated_message>"}, {...}]} \n'
+            + """Roles should not be translated! Just copy them from the source.
+                        Translate only the strings inside of the "message" attributes.
+                        Do not translate neither proper nouns nor proper names""",
+        ),
+        EasyInputMessageParam(role="user", content=json.dumps(transcript)),
+    ]
+
+    response = client.responses.create(
         model="gpt-4o-mini",
-        messages=[
-            {
-                "role": "system",
-                "content": f"""You are a professional translator, your role is to translate a given transcript of a voice call between AI agent and human to the language: {lang}. \n
-                            Only return the translated transcript in the JSON format, nothing else. \n """
-                + 'The JSON format should look like this: {"transcript":[{"role":"<role>", "message":"<translated_message>"}, {...}]} \n'
-                + """Roles should not be translated! Just copy them from the source.
-                            Translate only the strings inside of the "message" attributes.
-                            Do not translate neither proper nouns nor proper names""",
-            },
-            {"role": "user", "content": json.dumps(transcript)},
-        ],
+        input=input_messages,
         temperature=0.3,
+        text=ResponseTextConfigParam(
+            format=ResponseFormatTextJSONSchemaConfigParam(
+                name="translated messages", type="json_schema", schema=model_schema, strict=True
+            )
+        ),
     )
     res = response.choices[0].message.content.strip()
     return json.loads(res)
